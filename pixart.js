8ce154d16d98984c8e1812aa1abd4b16d765ff03
Frew A Negash <zulufanke@yahoo.com>
2017-07-10 08:19:19 -0400

    created function targeting box for color change

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+console.log('check-check!1-2-3!')
+var color = $('set-color')
+var btarget = $('.brush')
 
+function changeColor (evt) {
+  evt.preventDefault()
+btarget.css('background', 'red')
+}
+color.on('click', changeColor)
